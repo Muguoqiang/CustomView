5b88320993d6318d74ddbf74b025c0fa410ec583
Muguoqiang <550255407@qq.com>
2017-06-13 16:29:51 +0800

    2017年6月13日16:29:45

---
+++ view/src/main/java/example/view/AA.java
@@ -0,0 +1,23 @@
+package example.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+
+public class AA extends View {
+    public AA(Context context) {
+        super(context);
+    }
+
+    public AA(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AA(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
